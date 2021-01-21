63ceeee5d41cd6b44ed41bc7386e9673991a50f7
Mahadi Hasan <mahadihk@outlook.com>
2021-01-21 20:57:06 +0600

    Worked in writepanel, dashboards. Apexcharts and tinymce added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,42 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        loop:true,
-        responsiveClass:true,
-        autoWidth:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:true
-            },
-            600:{
-                items:2,
-                nav:false
-            },
-            1000:{
-                items:2,
-                nav:true
+    // Apex Charts
+    function showPostCount()
+    {
+        var postCount = {
+            series: [{
+              name: "Post",
+              data: [5, 3, 0, 1, 8, 2, 4]
+          }],
+            chart: {
+            height: 350,
+            type: 'line',
+            zoom: {
+              enabled: false
             }
-        }
-    });
+          },
+          dataLabels: {
+            enabled: false
+          },
+          stroke: {
+            curve: 'straight'
+          },
+          title: {
+            text: 'Post published by Day',
+            align: 'left'
+          },
+          grid: {
+            row: {
+              colors: ['#f3f3f3', 'transparent'],
+              opacity: 0.5
+            },
+          },
+          xaxis: {
+            categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+          }
+        };
+        var chart = new ApexCharts(document.querySelector("#chartPostCount"), postCount);
+        chart.render();
+    }  
+
+$(document).ready(function(){
+
 });
